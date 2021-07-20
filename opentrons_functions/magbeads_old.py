6b472ec40391ef6c9683746e310cd1e855700ca4
spencer-siddons <ssiddon@purdue.edu>
2021-07-20 12:47:32 -0700

    updated file names

--- opentrons_functions/magbeads_old.py
+++ opentrons_functions/magbeads_old.py
@@ -10,16 +10,12 @@ def bead_mix(pipette,
              n=5,
              z_offset=2,
              mix_vol=200,
-             mix_lift=0,
              drop_tip=False):
     for col in cols:
         pipette.pick_up_tip(tiprack.wells_by_name()[col])
         pipette.mix(n,
                     mix_vol,
                     plate[col].bottom(z=z_offset))
-        pipette.dispense(mix_vol,
-        			plate[col].bottom(z=z_offset + mix_lift))
-        			
         pipette.blow_out(plate[col].top())
 
         if drop_tip:
@@ -57,7 +53,8 @@ def remove_supernatant(pipette,
                              rate=rate)
             pipette.air_gap(10)
             pipette.dispense(transfer_vol + 10, waste.top())
-            vol_remaining -= transfer_vol#pipette.blow_out()
+            vol_remaining -= transfer_vol
+            pipette.blow_out()
         # we're done with these tips at this point
         pipette.blow_out()
         if drop_tip:
@@ -82,7 +79,6 @@ def bead_wash(  # global arguments
               # mix arguments
               mix_tiprack,
               # optional arguments
-              resuspend_beads=True,
               super_vol=600,
               rate=0.25,
               super_bottom_offset=2,
@@ -93,11 +89,8 @@ def bead_wash(  # global arguments
               wash_tip=None,
               wash_tip_vol=300,
               drop_wash_tip=True,
-              touch_wash_tip=True,
               mix_vol=200,
               mix_n=10,
-              mix_z_offset=2,
-              mix_lift=0,
               drop_mix_tip=False,
               mag_engage_height=None,
               pause_s=300):
@@ -124,10 +117,9 @@ def bead_wash(  # global arguments
                        rate=rate,
                        bottom_offset=super_bottom_offset,
                        drop_tip=drop_super_tip)
-	
-	if resuspend_beads:
-    	# disengage magnet
-    	magblock.disengage()
+
+    # disengage magnet
+    magblock.disengage()
 
     # This should:
     # - Pick up tips from column 3 of location 2
@@ -137,7 +129,7 @@ def bead_wash(  # global arguments
     # - dispense to `cols` in mag plate
     # - drop tips at end
 
-    # add wash -- changed from add isopropanol
+    # add isopropanol
     wash_wells, wash_remaining = add_buffer(pipette,
                                             source_wells,
                                             plate,
@@ -147,8 +139,7 @@ def bead_wash(  # global arguments
                                             tip=wash_tip,
                                             tip_vol=wash_tip_vol,
                                             remaining=remaining,
-                                            drop_tip=drop_wash_tip,
-                                            touch_tip=touch_wash_tip)
+                                            drop_tip=drop_wash_tip)
 
     # This should:
     # - grab a tip from position 8
@@ -157,26 +148,23 @@ def bead_wash(  # global arguments
     # - return tip
     # - do next col
     # - engage magnet
-	
-	if resuspend_beads:
-    	# mix
-    	bead_mix(pipette,
-            	 plate,
-            	 cols,
-            	 mix_tiprack,
-            	 n=mix_n,
-            	 mix_vol=mix_vol,
-            	 drop_tip=drop_mix_tip,
-            	 z_offset=mix_z_offset,
-                 mix_lift=mix_lift)
 
-    	# engage magnet
-    	if mag_engage_height is not None:
-        	magblock.engage(height_from_base=mag_engage_height)
-    	else:
-        	magblock.engage()
+    # mix
+    bead_mix(pipette,
+             plate,
+             cols,
+             mix_tiprack,
+             n=mix_n,
+             mix_vol=mix_vol,
+             drop_tip=drop_mix_tip)
 
-    	protocol.delay(seconds=pause_s)
+    # engage magnet
+    if mag_engage_height is not None:
+        magblock.engage(height_from_base=mag_engage_height)
+    else:
+        magblock.engage()
+
+    protocol.delay(seconds=pause_s)
 
     return(wash_wells, wash_remaining)
 
